e00dd9d56efccf78a9b46291ccf956f1a88d29f8
adindaaasals <adindaaasals28@gmail.com>
2024-06-07 21:24:44 +0700

    add dashboard

---
+++ category.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("./superstore.json")
+      .then(response => response.json())
+      .then(data => {
+        console.log('data: ', data)
+        let tableBody = document.getElementById("sales-table-body");
+        data.forEach(function(item) {
+          let row = document.createElement("tr");
+          row.innerHTML = `
+            <td>${item.category}</td>
+            <td>${item.sub_category}</td>
+            <td>${item.sales}</td>
+          `;
+          tableBody.appendChild(row);
+        });
+        $("#sales-table").DataTable();
+      });
+});
