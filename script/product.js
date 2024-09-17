4d426e8e95e751d3c6ba17679c17ae13b3bfc80b
adindaaasals <adindaaasals28@gmail.com>
2024-09-17 11:52:28 +0700

    final commit

--- script/product.js
+++ script/product.js
@@ -1,20 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("assets/js/superstore.json")
-      .then(response => response.json())
-      .then(data => {
-        console.log('data: ', data)
-        let tableBody2 = document.getElementById("product-table-body");
-        data.forEach(function(item) {
-          let row = document.createElement("tr");
-          row.innerHTML = `
-            <td>${item.product_name}</td>
-            <td>${item.sales}</td>
-          `;
-          tableBody2.appendChild(row);
-        }, {});
-        $("#product-table").DataTable();
-      });
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     fetch("assets/js/superstore.json")
+//       .then(response => response.json())
+//       .then(data => {
+//         console.log('data: ', data)
+//         let tableBody2 = document.getElementById("product-table-body");
+//         data.forEach(function(item) {
+//           let row = document.createElement("tr");
+//           row.innerHTML = `
+//             <td>${item.product_name}</td>
+//             <td>${item.sales}</td>
+//           `;
+//           tableBody2.appendChild(row);
+//         }, {});
+//         $("#product-table").DataTable();
+//       });
+// });
 
 
 // Cloning data for the second table
